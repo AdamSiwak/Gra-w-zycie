6344836d932461d9f0bccdc02353f1e3b4f45698
andrzej2124 <adziwinski@gmail.com>
2017-11-26 19:49:38 +0100

    First commit

--- applicationwindow.cpp
+++ applicationwindow.cpp
@@ -1,11 +1,14 @@
 #include "applicationwindow.h"
 #include "ui_applicationwindow.h"
+#include <QLabel>
 
 ApplicationWindow::ApplicationWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ApplicationWindow)
 {
     ui->setupUi(this);
+    QLabel *label = new QLabel(this);
+    label->setText("Adam \nAndrzej");
 }
 
 ApplicationWindow::~ApplicationWindow()
